6aa13a4537582c4a9a073e2b4d50322909195d7a
Mohit Soni <developer_mohitsoni@outlook.com>
2024-10-18 17:11:08 +0530

    Kanban Column Task

--- src/components/KanbanBoard.tsx
+++ src/components/KanbanBoard.tsx
@@ -1,11 +1,50 @@
+import { useState } from "react";
 import PlusIcon from "../icons/PlusIcon";
+import { Column, Id } from "../types";
+import ColumnContainer from "./ColumnContainer";
 
 const KanbanBoard = () => {
+  const [columns, setColumns] = useState<Column[]>([]);
+
+  console.log(columns);
+
+  const generateId = () => {
+    // Generate a random number between 0 and 10000
+
+    return Math.floor(Math.random() * 10001);
+  };
+
+  const createNewColumn = () => {
+    const columnToAdd: Column = {
+      id: generateId(),
+      title: `Column ${columns.length + 1}`,
+    };
+
+    setColumns([...columns, columnToAdd]);
+  };
+
+  const deleteColumn = (id: Id) => {
+    const filteredColumns = columns.filter((col) => col.id !== id);
+
+    setColumns(filteredColumns);
+  };
   return (
     <div className="m-auto flex min-h-screen w-full items-center overflow-x-auto overflow-y-hidden px-[40px]">
-      <div className="m-auto">
+      <div className="m-auto flex gap-4">
+        <div className="flex gap-4">
+          {columns.map((col) => (
+            <ColumnContainer
+              key={col.id}
+              column={col}
+              deleteColumn={deleteColumn}
+            />
+          ))}
+        </div>
         <button
-          className="h-[60px] w-[350px] min-w-[350px] cursor-pointer rounded-lg bg-mainBackgroundColor border-2 border-columnBackgroundColor p-4 ring-rose-500 hover:ring-2 gap-2"
+          className="h-[60px] w-[350px] min-w-[350px] cursor-pointer rounded-lg bg-mainBackgroundColor border-2 border-columnBackgroundColor p-4 ring-rose-500 hover:ring-2 flex gap-2"
+          onClick={() => {
+            createNewColumn();
+          }}
         >
           <PlusIcon /> Add Column
         </button>
